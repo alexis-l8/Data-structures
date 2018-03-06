7dfce169bd1eed2ef16b18a02a6d2f1368f8d6e2
Alexis Lui <alexislui@hotmail.co.uk>
2018-03-06 00:26:51 +0000

    Iterative solution to fibonacci sequence

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,20 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let sequence = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i === 0) {
+      sequence.push(0);
+    } else if (i === 1) {
+      sequence.push(1);
+    } else {
+      sequence.push(sequence[i - 2] + sequence[i - 1]);
+    }
+  }
+
+  return sequence[n];
+}
 
 module.exports = fib;
